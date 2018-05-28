11fd56894dabc231b3442b60506326c7e35e7028
tonyyzy <38984697+tonyyzy@users.noreply.github.com>
2018-05-28 17:54:38 +0100

    Satisfy pylint

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,8 +1,13 @@
-from scripts import GC_analysis
+"""
+Test_1
+"""
+
 import filecmp
+from scripts import GC_analysis
 
 
 def test_1():
+    """Test_1"""
     GC_analysis.generate_wiggle("./tests/example.fasta", "test_1.wig", 5, 5)
     print(filecmp.cmp("./tests/test_1.wig", "./tests/example.wig"))
 
