cbd4d2e50e349cf870317e6eedf545b988853abb
tonyyzy <38984697+tonyyzy@users.noreply.github.com>
2018-06-07 13:46:16 +0100

    Add test

--- tests/test_error_ex3.py
+++ tests/test_error_ex3.py
@@ -9,7 +9,7 @@ from scripts import GC_analysis
 def test_3(capsys):
     """Test_1"""
     with pytest.raises(SystemError):
-        GC_analysis.generate_wiggle("./tests/ex3.fasta", "ex3.wig", 5, 5)
+        GC_analysis.generate_wiggle("./tests/ex3.fasta", "ex3.fasta.wig", 5, 5)
     out, err = capsys.readouterr()
     assert out == "WARNING! The input file is not in fasta format.\n"
 
