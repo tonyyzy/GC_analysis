09d88e513bb75f5006097f4cb1cd66ed0b49afed
tonyyzy <38984697+tonyyzy@users.noreply.github.com>
2018-06-08 23:11:44 +0100

    Remove unused variable

--- tests/test_error_ex3.py
+++ tests/test_error_ex3.py
@@ -10,8 +10,8 @@ def test_3(capsys):
     """Test_1"""
     with pytest.raises(SystemError):
         GC_analysis.generate_wiggle("./tests/ex3.fasta", "ex3.fasta.wig", 5, 5)
-    out, err = capsys.readouterr()
+    out = capsys.readouterr()[0]
     assert out == "WARNING! The input file is not in fasta format.\n"
 
 if __name__ == "__main__":
-    test_3()
+    test_3(capsys)
