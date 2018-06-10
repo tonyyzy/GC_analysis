a835dd763bf67cfe1f8667bb0b0660b39b5420c3
tonyyzy <38984697+tonyyzy@users.noreply.github.com>
2018-06-11 00:28:33 +0100

    Add gzip test

---
+++ tests/test_file_ex2_5_3_gz.py
@@ -0,0 +1,24 @@
+"""
+Test_1
+"""
+
+import filecmp
+import subprocess
+
+
+def test_1():
+    """Test_1"""
+    result = subprocess.run(["python3", "./scripts/GC_analysis.py",
+                             "-i", "./tests/ex2.fasta",
+                             "-o", "./tests/ex2.fasta.wig.gz",
+                             "-w", "5",
+                             "-s", "3",
+                             "-f", "gzip"],
+                            stderr=subprocess.PIPE)
+    subprocess.run(["gzip", "-d", "ex2.fasta.wig.gz"])
+    assert filecmp.cmp("./tests/ex2.fasta.wig", "./tests/ex2_5_3.wig")
+    assert result.stderr == b"WARNING! This fasta file does not contain chromosome information.\n"
+
+
+if __name__ == "__main__":
+    test_1()
