ee31abc8d45d085e8d38fe66d1e6c7bd89d9a3e1
tonyyzy <38984697+tonyyzy@users.noreply.github.com>
2018-06-11 11:57:09 +0100

    Update tests

--- tests/test_file_ex1_5_5.py
+++ tests/test_file_ex1_5_5.py
@@ -8,11 +8,11 @@ import subprocess
 
 def test_1():
     """Test_1"""
-    result = subprocess.run(["python3", "./scripts/GC_analysis.py",
+    subprocess.run(["python3", "./scripts/GC_analysis.py",
                              "-i", "./tests/ex1.fasta",
                              "-o", "./tests/ex1.fasta.wig",
                              "-w", "5",
-                             "-s", "5"], stderr=subprocess.PIPE)
+                             "-s", "5"])
     assert filecmp.cmp("./tests/ex1.fasta.wig", "./tests/ex1_5_5.wig")
 
 
--- tests/test_file_ex2_5_3_gz.py
+++ tests/test_file_ex2_5_3_gz.py
@@ -8,13 +8,12 @@ import subprocess
 
 def test_1():
     """Test_1"""
-    result = subprocess.run(["python3", "./scripts/GC_analysis.py",
+    subprocess.run(["python3", "./scripts/GC_analysis.py",
                              "-i", "./tests/ex2.fasta",
                              "-o", "./tests/ex2.fasta.wig.gz",
                              "-w", "5",
                              "-s", "3",
-                             "-f", "gzip"],
-                            stderr=subprocess.PIPE)
+                             "-f", "gzip"])
     subprocess.run(["gzip", "-d", "ex2.fasta.wig.gz"])
     assert filecmp.cmp("./tests/ex2.fasta.wig", "./tests/ex2_5_3.wig")
 
