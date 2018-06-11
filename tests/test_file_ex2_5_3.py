9ebb585a52d720e683b475cfcbf7703509d088a5
tonyyzy <38984697+tonyyzy@users.noreply.github.com>
2018-06-11 12:00:50 +0100

    Update tests

--- tests/test_file_ex2_5_3.py
+++ tests/test_file_ex2_5_3.py
@@ -8,11 +8,11 @@ import subprocess
 
 def test_1():
     """Test_1"""
-    result = subprocess.run(["python3", "./scripts/GC_analysis.py",
+    subprocess.run(["python3", "./scripts/GC_analysis.py",
                              "-i", "./tests/ex2.fasta",
                              "-o", "./tests/ex2.fasta.wig",
                              "-w", "5",
-                             "-s", "3"], stderr=subprocess.PIPE)
+                             "-s", "3"])
     assert filecmp.cmp("./tests/ex2.fasta.wig", "./tests/ex2_5_3.wig")
 
 
