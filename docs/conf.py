1254dd3be04b5a138ab7e7378a53bfb61fd8b003
tonyyzy <38984697+tonyyzy@users.noreply.github.com>
2018-06-13 16:36:25 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,11 +14,16 @@
 #
 import os
 import sys
-from mock import Mock
-
-sys.modules['pyBigWig'] = Mock()
-
 sys.path.insert(0, os.path.abspath('../'))
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['pyBigWig']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 # -- Project information -----------------------------------------------------
 
 project = 'GC_analysis'
